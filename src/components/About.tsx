8b2cfbfff5acfcb0bc4080f98d4ad92934c771d6
Prince Gajjar <princegajjar8002@gmail.com>
2026-06-15 17:09:39 +0530

    feat: add GEO-optimized statistics highlights grid to About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,6 +9,26 @@ const About = () => {
         <p className="para">
           {config.about.description}
         </p>
+        
+        {/* GEO-Optimized Highlights Stats Grid */}
+        <div className="about-highlights" data-cursor="disable">
+          <div className="highlight-item">
+            <span className="highlight-number">11+</span>
+            <span className="highlight-label">Systems Delivered</span>
+          </div>
+          <div className="highlight-item">
+            <span className="highlight-number">30%+</span>
+            <span className="highlight-label">Bug Reduction</span>
+          </div>
+          <div className="highlight-item">
+            <span className="highlight-number">95+</span>
+            <span className="highlight-label">PageSpeed Score</span>
+          </div>
+          <div className="highlight-item">
+            <span className="highlight-number">100%</span>
+            <span className="highlight-label">Client Trust</span>
+          </div>
+        </div>
       </div>
     </section>
   );
